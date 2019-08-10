6038482cec983b58e39130d5ba7a62db642900df
cestevezing <47781439+cestevezing@users.noreply.github.com>
2019-08-09 20:44:02 -0500

    modelado

---
+++ Trabajo/src/main/java/com/mio/model/Message.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mio.model;
+
+/**
+ *
+ * @author HTTP
+ */
+public class Message {
+    
+    private int id;
+    private String content;
+    
+    public Message(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    
+}
